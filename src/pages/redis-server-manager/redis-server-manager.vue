6be56113094d69c180d85a982bfd2332c108e900
RobinWeipengChen <dayuqi@yeah.net>
2017-02-17 18:36:19 +0800

    feature: 提交一点UI

--- src/pages/redis-server-manager/redis-server-manager.vue
+++ src/pages/redis-server-manager/redis-server-manager.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="background-div">
     <p class="title">New Connection</p>
-    <div class = "rowContainer">
+    <div class = "row-container">
         <button class="tab-bar-button">Connection</button>
         <button class="tab-bar-button">SSL</button>
         <button class="tab-bar-button">SSH</button>
         <button class="tab-bar-button">Advanced Option</button>
     </div>
-    <div  class = "rowContainer">
-        <button class="tab-bar-button">Connection</button>
-        <textarea class="tab-bar-button">Connection</textarea>
-
-            <button class="tab-bar-button">Connection</button>
+    <div  class = "row-container">
+        <button class="left-button">Connection</button>
+        <textarea class="content">Connection</textarea>
+           <span class = "column-container">
+             <button class="tab-bar-button">Add</button>
+             <button class="tab-bar-button">Remove</button>
+             <button class="tab-bar-button">AutoDetect</button>
+           </span>
 
     </div>
   </div>
@@ -26,18 +29,31 @@
       background-color: green;
       width: 800px;
       height:1000px;
+
    }
-   .rowContainer
+   .column-container
+   {
+      display: flex;
+      margin-left: 15px;
+      margin-right: 15px;
+      flex-direction: column;
+
+   }
+   .row-container
    {  display: flex;
       flex-direction: row;
       margin-left: 15px;
       margin-right: 15px;
       margin-top: 15px;
+      justify-content: space-between;
    }
 
 
    .title{ color: white;      margin-left: 15px;}
-   .tab-bar-button{ flex: 1; height:40px;}
+   .tab-bar-button{ flex: 1; height:40px; margin-right: 5px;}
+   .left-button{ flex: 1; height:40px; margin-right: 15px;}
+   .content{ flex: 1; height:240px; margin-right: 15px;}
+
 </style>
 
 <script>
