3bf25b4aea8ab2099610e3a6d64e8ec04b0c6de8
wangxiao <wanngxiao@gmail.com>
2017-02-17 16:34:13 +0800

    feat: 添加 libs alias

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -25,7 +25,8 @@ module.exports = {
     alias: {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
+      'components': path.resolve(__dirname, '../src/components'),
+      'libs': path.resolve(__dirname, '../src/libs')
     }
   },
   resolveLoader: {
